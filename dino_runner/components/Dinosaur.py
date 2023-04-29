e95d26bd351dbd1decb2424d38e033dbbb3532fd
Santyartcp <Santiago.Chacon@fundacion-jala.org>
2023-04-28 19:12:25 -0500

    Class 2:Dinosaur running

---
+++ dino_runner/components/Dinosaur.py
@@ -0,0 +1,52 @@
+import pygame
+from pygame.sprite import Sprite
+from dino_runner.utils.constants import RUNNING, DUCKING
+
+class Dinosaur(Sprite):
+    POS_X = 80
+    POS_Y = 310
+
+    def __init__(self):
+        self.image = RUNNING [0]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.POS_X
+        self.dino_rect.y = self.POS_Y
+        self.setp_index = 0
+
+    def update(self, user_input):
+        if user_input[pygame.K_DOWN]:
+            self.duck()
+        else:
+            self.run()
+
+        if self.setp_index >= 10:
+            self.setp_index = 0
+
+    def draw(self, screen):
+        screen.blit(self.image, self.dino_rect)
+
+    def run(self):
+        self.image = RUNNING [0] if self.setp_index < 5 else RUNNING[1]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.POS_X
+        self.dino_rect.y = self.POS_Y 
+
+        self.setp_index += 1
+
+    def duck(self):
+        if  self.setp_index < 5:
+            self.image = DUCKING[0]
+        else:
+            self.image = DUCKING[1]
+
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.POS_X
+        self.dino_rect.y = 350 
+
+        self.setp_index += 1
+
+
+
+    
+
+
